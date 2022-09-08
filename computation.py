19f581888e8381037d9703cf2be084b5180f5650
gabiga <37190037+gabiga7@users.noreply.github.com>
2022-09-08 14:19:22 +0200

    Update and add of yellow&red bar notion

--- computation.py
+++ computation.py
@@ -2,6 +2,10 @@ import time
 import numpy as np
 
 
+def ajustement_depense_bj(ftp,puissance,depense):
+    #print(1-1/(np.exp(duree_tenable(ftp,puissance)/(ftp/3))))
+    return depense*(1-1/(np.exp(duree_tenable(ftp,puissance)/(ftp/3))))
+
 
 def puissance_tenable(ftp,temps):
     return 1900/np.log(temps)+15*np.log(temps)+ftp-354.858
@@ -18,24 +22,41 @@ def duree_tenable(ftp,puissance):
 
 
 
-def depense_puissance_duree(ftp,puissance,temps):
+def depense_puissance_duree_bj(ftp,puissance,temps):
     if puissance<=0.55*ftp:
          return -temps*0.02
     if puissance<=0.75*ftp:
         return temps*0.007
     if puissance<=0.89*ftp:
         return temps*0.014
-    return (temps*puissance/(duree_tenable(ftp,puissance)*puissance_tenable(ftp,duree_tenable(ftp,puissance))))*100
+    depense=(temps*puissance/(duree_tenable(ftp,puissance)*puissance_tenable(ftp,duree_tenable(ftp,puissance))))*100
+    #print("depense de base  : ",depense)
+    if puissance>=1.1*ftp:
+        return temps*ajustement_depense_bj(ftp,puissance,depense)
+    return depense
 
-pt=puissance_tenable(ftp=300,temps=3600)
-dt=duree_tenable(ftp=300,puissance=300)
-dpd=depense_puissance_duree(ftp=350,puissance=270,temps=1)
+def depense_puissance_duree_br(ftp,puissance,temps):
+    if puissance<=0.9*ftp:
+        return -temps*0.083
+    if puissance>=1.1*ftp:
+        return (temps*puissance/(duree_tenable(ftp,puissance)*puissance_tenable(ftp,duree_tenable(ftp,puissance))))*100
+    return 0
 
 
-print("puissance tenable sur 1h =",pt,"w")
-print("duree tenable avec 300w =",dt,"sec")
-print("depense avec 300w sur 60min =",dpd,"pv depenses sur 100")
-print("~~~~~~~~~~~~")
+pt=puissance_tenable(ftp=300,temps=300)
+dt=duree_tenable(ftp=300,puissance=1050)
+
+#dpd=depense_puissance_duree_bj(ftp=300,puissance=325,temps=1)
+#print("depense avec =",dpd,"pv depenses sur 100")
+#print("~~~~")
+dpd=depense_puissance_duree_br(ftp=300,puissance=200,temps=1)
+#print("depense avec =",dpd,"pv depenses sur 100")
+
+
+#print("puissance tenable =",pt,"w")
+#print("duree tenable =",dt,"sec")
+#print("depense avec =",dpd,"pv depenses sur 100")
+#print("~~~~~~~~~~~~")
 print()
 
 
@@ -58,12 +79,19 @@ bloc_16=[800]*5
 bloc_17=[1100]*3
 
 
-def ajustement_energie(energie,depense):
-    if energie-depense>100:
+def ajustement_energie_bj(energie_bj,depense):
+    if energie_bj-depense>100:
         return 100
-    if energie-depense<=0:
+    if energie_bj-depense<=0:
         return 0
-    return energie-depense
+    return energie_bj-depense
+
+def ajustement_energie_br(energie_br,depense):
+    if energie_br-depense>100:
+        return 100
+    if energie_br-depense<=0:
+        return 0
+    return energie_br-depense
 
 
 
@@ -89,14 +117,23 @@ def main():
     gpx+=(bloc_16)
     gpx+=(bloc_17)
 
-    energie=100
+    energie_bj=100
+    energie_br=100
     print(len(gpx),"sec")
     sec=0
     for i in gpx:
-        energie=ajustement_energie(energie,depense_puissance_duree(ftp=277,puissance=i,temps=1))
-        if energie==0:
-            print("Ne pas craquer")
-        print(sec,"sec ",i,"w ",energie,"pv")
+        energie_bj=ajustement_energie_bj(energie_bj,depense_puissance_duree_bj(ftp=277,puissance=i,temps=1))
+        energie_br=ajustement_energie_br(energie_br,depense_puissance_duree_br(ftp=277,puissance=i,temps=1))
+        if energie_bj==0:
+            print("Vous avez besoin de récupérer de la barre jaune")
+        if energie_br==0:
+            print("Vous avez besoin de récupérer de la barre rouge")
+        print("~~")
+        print(sec,"sec")
+        print(i,"w")
+        print("barre jaune : ",energie_bj,"pv")
+        print("barre rouge : ",energie_br,"pv")
+
         sec+=1
         time.sleep(0.1)
 
